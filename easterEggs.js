bfbc1a33fa95bfcd24d7ca95c710797b50187bff
mrHumildad <nghizzah@gmail.com>
2023-03-04 14:45:25 +0100

    adding styles to pdf version

--- easterEggs.js
+++ easterEggs.js
@@ -7,6 +7,19 @@ let ageMs = now - birthDate
 daysMs = ageMs % 31536000000 
 years.innerText = Math.round(ageMs/31536000000)
 days.innerText = Math.round(daysMs/86400000)
-///
+///export a dynamic verion of page as downlodable pdf
+function makePdf() {
+    const { jsPDF } = window.jspdf;
 
+    var doc = new jsPDF('l', 'mm', [1500, 1400]);
+    var pdfjs = document.querySelector('#divID');
+
+    doc.html(pdfjs, {
+        callback: function(doc) {
+            doc.save("newpdf.pdf");
+        },
+        x: 12,
+        y: 12
+    });               
+} 
 
