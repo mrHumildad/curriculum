287575a3753fc20a99ab49bdcd9363b329ea3330
mrHumildad <nghizzah@gmail.com>
2024-09-25 18:52:09 +0200

    correcting errors

--- easterEggs.js
+++ easterEggs.js
@@ -1,19 +1,10 @@
-///// calculate age in years and days
-const birthDate = new Date(253536050000)
-const now = Date.now()
-const years = document.getElementById("years")
-const days = document.getElementById("days")
-let ageMs = now - birthDate
-daysMs = ageMs % 31536000000 
-years.innerText = Math.round(ageMs/31536000000)
-days.innerText = Math.round(daysMs/86400000)
-///export a dynamic verion of page as downlodable pdf
+
+
+///export a dynamic version of page as downlodable pdf
 function makePdf() {
     const { jsPDF } = window.jspdf;
-
     var doc = new jsPDF('l', 'mm', [1500, 1400]);
     var pdfjs = document.querySelector('#divID');
-
     doc.html(pdfjs, {
         callback: function(doc) {
             doc.save("newpdf.pdf");
