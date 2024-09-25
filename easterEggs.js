61498079cf9f1289f72f43fb527c93b541a8219e
mrHumildad <nghizzah@gmail.com>
2024-09-25 19:17:45 +0200

    added easter egg

--- easterEggs.js
+++ easterEggs.js
@@ -1,4 +1,13 @@
+function caesarCipher(str, shift) {  let result = "";  for (let i = 0; i < str.length; i++) {    let charCode = str.charCodeAt(i);    if (charCode >= 65 && charCode <=  90) {      result += String.fromCharCode((charCode - 65 + shift) % 26 + 65);    } else if (charCode >= 97 && charCode <= 122) {      result += String.fromCharCode((charCode - 97 + shift) % 26 + 97);       } else {      result += str[i];     }  }   return result;}
 
+const searchParams = new URLSearchParams(window.location.search);
+if (searchParams.has('x')) {
+    const yourCompany = document.getElementById('your-company');
+    const companyName = caesarCipher(searchParams.get('x'), -3);
+    yourCompany.innerText = companyName
+}
+
+console.log(caesarCipher('lalluvia.com', 3))
 
 ///export a dynamic version of page as downlodable pdf
 function makePdf() {
