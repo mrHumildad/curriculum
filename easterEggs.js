1c6df017fe1318626da300ce0da3a80854190008
mrHumildad <nghizzah@gmail.com>
2024-09-29 16:18:39 +0200

    implementing icons and multilanguage

--- easterEggs.js
+++ easterEggs.js
@@ -7,8 +7,6 @@ if (searchParams.has('x')) {
     yourCompany.innerText = companyName
 }
 
-console.log(caesarCipher('lalluvia.com', 3))
-
 ///export a dynamic version of page as downlodable pdf
 function makePdf() {
     const { jsPDF } = window.jspdf;
@@ -21,5 +19,41 @@ function makePdf() {
         x: 12,
         y: 12
     });               
-} 
+}
+const langButt = document.getElementById('traducer');
+function getBeforeQuestionMark(str) {
+    const questionIndex = str.indexOf('?'); // Find the position of the '?'
+    
+    if (questionIndex === -1) {
+        return str; // If '?' is not found, return the whole string
+    }
+    
+    return str.substring(0, questionIndex); // Return the string from the beginning to the '?'
+}
+
+// Function to return the string from the first occurrence of '?' to the end
+function getAfterQuestionMark(str) {
+    const questionIndex = str.indexOf('?'); // Find the position of the '?'
+    
+    if (questionIndex === -1) {
+        return ''; // If '?' is not found, return an empty string
+    }
+    
+    return str.substring(questionIndex); // Return the string from '?' to the end
+}
+const swapLang = () => {
+    const URL = window.location.href;
+    lang = document.documentElement.lang;
+    const x = getAfterQuestionMark(URL);
+    const cleanURL = document.URL.substr(0,document.URL.lastIndexOf('/'));
+		console.log(cleanURL)
+    /* let array = ['en', 'es', 'it', 'ca'];
+    let suffix = ['', 'index_ES.html', '_IT', '_CA']
+    let index = array.indexOf(lang);
+    if (index === array.length - 1) {
+      index = 0
+    } else index++;
+		window.location.href = cleanURL + suffix[index] + x; */
+}
+langButt.addEventListener('click', swapLang)
 
